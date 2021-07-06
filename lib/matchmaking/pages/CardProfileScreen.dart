3b53f51cec2a0f097f102aa4c25e841bf22a2542
Ashley Guevara <ashleyngp@gmail.com>
2021-07-06 16:50:55 -0500

    Fix main style

--- lib/matchmaking/pages/CardProfileScreen.dart
+++ lib/matchmaking/pages/CardProfileScreen.dart
@@ -1,9 +1,11 @@
+import 'package:Okuna/matchmaking/constants.dart';
+import 'package:Okuna/matchmaking/widgets/Badge.dart';
 import 'package:Okuna/models/badge.dart';
 import 'package:Okuna/models/communities_list.dart';
 import 'package:Okuna/models/community.dart';
 import 'package:Okuna/models/theme.dart';
 import 'package:Okuna/models/user.dart';
-import 'package:Okuna/pages/home/pages/community/widgets/community_card/community_card.dart';
+import 'package:Okuna/matchmaking/model/User.dart' as tinder;
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/profile_bio.dart';
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/profile_connected_in.dart';
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/profile_connection_request.dart';
@@ -20,14 +22,14 @@ import 'package:Okuna/services/user.dart';
 import 'package:Okuna/widgets/avatars/avatar.dart';
 import 'package:Okuna/widgets/nav_bars/themed_nav_bar.dart';
 import 'package:Okuna/widgets/theming/primary_color_container.dart';
-import 'package:Okuna/widgets/tiles/community_tile.dart';
 import 'package:Okuna/widgets/user_badge.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CardProfileScreen extends StatefulWidget {
-  final User user;
-  const CardProfileScreen({ Key key, this.user }) : super(key: key);
+  final tinder.User user;
+
+  const CardProfileScreen({ Key key, @required this.user}) : super(key: key);
 
   @override
   _CardProfileScreenState createState() => _CardProfileScreenState();
@@ -36,18 +38,32 @@ class CardProfileScreen extends StatefulWidget {
 class _CardProfileScreenState extends State<CardProfileScreen> {
   static User _user;
   UserService _userService;
+  bool _needsBootstrap;
+  bool _isLoading;
 
   @override
   void initState() {
-    setState(() {
-      _user = widget.user;
-    });
+    _needsBootstrap = true;
+    _isLoading = true;
     super.initState();
   }
+
   List<Container> _buildCommunitiesList(){
     List<Container> _communities = [
-      Container(padding: EdgeInsets.only(right: 10),width: 250, child: OBCommunityTile(Community.fromJSON({"id": 16, "creator": null, "name": "ash", "type": null, "rules": null, "avatar": null, "title": "AshCommunity", "user_adjective": null, "users_adjective": null, "description": null, "color": "#5a98ae", "cover": null, "is_invited": null, "are_new_post_notifications_enabled": null, "is_creator": null, "is_reported": null, "moderators": null, "memberships": [{"id": 34, "user_id": 19, "community_id": 16, "is_administrator": false, "is_moderator": false}], "administrators": null, "is_favorite": null, "invites_enabled": null, "members_count": null, "posts_count": 2, "pending_moderated_objects_count": null, "categories": null}))),
-      Container(padding: EdgeInsets.only(right: 10),width: 250, child: OBCommunityTile(Community.fromJSON({"id": 14, "creator": null, "name": "testcamilo", "type": null, "rules": null, "avatar": null, "title": "TestCamilo", "user_adjective": "Member", "users_adjective": "Members", "description": null, "color": "#96C0FB", "cover": null, "is_invited": null, "are_new_post_notifications_enabled": null, "is_creator": null, "is_reported": null, "moderators": null, "memberships": null, "administrators": null, "is_favorite": false, "invites_enabled": null, "members_count": 3, "posts_count": null, "pending_moderated_objects_count": null, "categories": null})))
+      Container(
+        padding: EdgeInsets.only(right: 10),
+        child: TextBadge(
+          community: Community.fromJSON({"id": 16, "creator": null, "name": "ash", "type": null, "rules": null, "avatar": null, "title": "AshCommunity", "user_adjective": null, "users_adjective": null, "description": null, "color": "#5a98ae", "cover": null, "is_invited": null, "are_new_post_notifications_enabled": null, "is_creator": null, "is_reported": null, "moderators": null, "memberships": [{"id": 34, "user_id": 19, "community_id": 16, "is_administrator": false, "is_moderator": false}], "administrators": null, "is_favorite": null, "invites_enabled": null, "members_count": null, "posts_count": 2, "pending_moderated_objects_count": null, "categories": null}
+          )
+        )
+      ),
+      Container(
+        padding: EdgeInsets.only(right: 10),
+        child: TextBadge(
+          community: Community.fromJSON({"id": 14, "creator": null, "name": "testcamilo", "type": null, "rules": null, "avatar": null, "title": "TestCamilo", "user_adjective": "Member", "users_adjective": "Members", "description": null, "color": "#96C0FB", "cover": null, "is_invited": null, "are_new_post_notifications_enabled": null, "is_creator": null, "is_reported": null, "moderators": null, "memberships": null, "administrators": null, "is_favorite": false, "invites_enabled": null, "members_count": 3, "posts_count": null, "pending_moderated_objects_count": null, "categories": null}
+          )
+        )
+      ),
     ];
 
     return _communities;
@@ -55,20 +71,35 @@ class _CardProfileScreenState extends State<CardProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+
     var openbookProvider = OpenbookProvider.of(context);
     var themeService = openbookProvider.themeService;
     var themeValueParserService = openbookProvider.themeValueParserService;
     var toastService = openbookProvider.toastService;
 
-    
+    Future<void> _getOkunaUser(String id) async {
+      var user = await _userService.getUserWithId('mike');
+      setState(() {
+        _user = user;
+        _isLoading = false;
+      });
+    }
 
-    return Container(
+    if (_needsBootstrap) {
+      _userService = openbookProvider.userService;
+      _needsBootstrap = false;
+      _getOkunaUser(widget.user.userID);
+    }
+
+    return _isLoading == true
+    ? LoadingContainer() 
+    : Container(
       child: Center(
         child: ClipRRect(
           borderRadius: BorderRadius.circular(25),
           child: CupertinoPageScaffold(
           backgroundColor: Color.fromARGB(0, 0, 0, 0),
-          navigationBar: OBThemedNavigationBar(title: '@' + _user.username),
+          navigationBar: OBThemedNavigationBar(title: '@' + widget.user.email),
           child: OBPrimaryColorContainer(
             child: Column(
               children:[
@@ -92,29 +123,57 @@ class _CardProfileScreenState extends State<CardProfileScreen> {
                               ),
                                _buildNameRow(user: _user, context: context, toastService: toastService),
                               OBProfileUsername(_user),
-                              Container( height: 65,child: OBProfileBio(_user)),
+
+                              Container( 
+                                height: 65,
+                                child: OBProfileBio(_user)
+                              ),
+                              const Divider(
+                                height: 20,
+                              ),
+                              Container(
+                                height: 50,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('COMMUNITIES'),
+                                    const SizedBox( height: 5),
+                                    Container(
+                                      height: 30,
+                                      child: ListView(
+                                        scrollDirection: Axis.horizontal,
+                                        children: _buildCommunitiesList(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const Divider(
+                                height: 20,
+                              ),
+                              Container(
+                                height: 50,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('GROUPS'),
+                                    const SizedBox( height: 5),
+                                    Container(
+                                      height: 30,
+                                      child: ListView(
+                                        scrollDirection: Axis.horizontal,
+                                        children: _buildCommunitiesList(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                               OBProfileDetails(_user),
                               OBProfileCounts(_user),
                               OBProfileConnectedIn(_user),
                               OBProfileConnectionRequest(_user),
                               OBProfileFollowRequest(_user),
                               OBProfileInLists(_user),
-                              Container(
-                                height: 60,
-                                margin: EdgeInsets.only(top: 10),
-                                child: ListView(
-                                  scrollDirection: Axis.horizontal,
-                                  children: _buildCommunitiesList(),
-                                ),
-                              ),
-                              Container(
-                                height: 60,
-                                margin: EdgeInsets.only(top: 10),
-                                child: ListView(
-                                  scrollDirection: Axis.horizontal,
-                                  children: _buildCommunitiesList(),
-                                ),
-                              ),
                               
                           ]),
                         ],
@@ -204,5 +263,23 @@ class _CardProfileScreenState extends State<CardProfileScreen> {
   String _getUserBadgeDescription(User user) {
     Badge badge = user.getProfileBadges()[0];
     return badge.getKeywordDescription();
+  }
+}
+
+class LoadingContainer extends StatelessWidget {
+  const LoadingContainer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor:
+              AlwaysStoppedAnimation<Color>(Color(COLOR_ACCENT)),
+        ),
+      ),
+    );
   }
 }
