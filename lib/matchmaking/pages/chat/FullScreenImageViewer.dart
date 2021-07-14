ae4495f7cc4980f49e2d075497224a3669dd4651
Ashley Guevara <ashleyngp@gmail.com>
2021-07-14 17:02:21 -0500

    Fix Full Image Viewer

--- lib/matchmaking/pages/chat/FullScreenImageViewer.dart
+++ lib/matchmaking/pages/chat/FullScreenImageViewer.dart
@@ -1,5 +1,6 @@
+import 'package:Okuna/widgets/progress_indicator.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
 
 class FullScreenImageViewer extends StatelessWidget {
   final String imageUrl;
@@ -17,11 +18,32 @@ class FullScreenImageViewer extends StatelessWidget {
           iconTheme: IconThemeData(color: Colors.white),
         ),
         body: Container(
+          height: MediaQuery.of(context).size.height,
           color: Colors.black,
           child: Hero(
             tag: imageUrl,
-            child: PhotoView(
-              imageProvider: NetworkImage(imageUrl),
+            child: ExtendedImage.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              cache: true,
+              loadStateChanged: (ExtendedImageState state) {
+                switch (state.extendedImageLoadState) {
+                  case LoadState.loading:
+                    return Center(child: OBProgressIndicator());
+                    break;
+                  case LoadState.completed:
+                    return ExtendedRawImage(
+                      image: state.extendedImageInfo?.image,
+                    );
+                    break;
+                  case LoadState.failed:
+                    return Image.asset('assets/images/matchmaking/error_image.png');
+                    break;
+                  default:
+                    return Image.asset('assets/images/matchmaking/img_placeholder.png');
+                    break;  
+                }
+              },
             ),
           ),
         ));
