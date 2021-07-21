59b43c05a968484061a0baf35a4f372b32008fcd
Ashley Guevara <ashleyngp@gmail.com>
2021-07-21 14:19:05 -0500

    Fix card profile screen

--- lib/matchmaking/pages/CardProfileScreen.dart
+++ lib/matchmaking/pages/CardProfileScreen.dart
@@ -1,11 +1,11 @@
 import 'package:Okuna/matchmaking/constants.dart';
+import 'package:Okuna/matchmaking/model/ProfileCardUser.dart';
 import 'package:Okuna/matchmaking/widgets/Badge.dart';
 import 'package:Okuna/models/badge.dart';
 import 'package:Okuna/models/categories_list.dart';
 import 'package:Okuna/models/community_membership_list.dart';
 import 'package:Okuna/models/theme.dart';
 import 'package:Okuna/models/user.dart';
-import 'package:Okuna/matchmaking/model/User.dart' as tinder;
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/profile_bio.dart';
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/profile_counts/profile_counts.dart';
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/profile_details/profile_details.dart';
@@ -14,29 +14,28 @@ import 'package:Okuna/pages/home/pages/profile/widgets/profile_card/widgets/prof
 import 'package:Okuna/pages/home/pages/profile/widgets/profile_cover.dart';
 import 'package:Okuna/provider.dart';
 import 'package:Okuna/services/toast.dart';
-import 'package:Okuna/services/user.dart';
 import 'package:Okuna/widgets/avatars/avatar.dart';
+import 'package:Okuna/widgets/theming/actionable_smart_text.dart';
 import 'package:Okuna/widgets/theming/primary_color_container.dart';
 import 'package:Okuna/widgets/user_badge.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CardProfileScreen extends StatefulWidget {
-  final tinder.User user;
+  final ProfileCardUser extendedUser;
 
-  const CardProfileScreen({ Key key, @required this.user}) : super(key: key);
+
+  const CardProfileScreen({ Key key, @required this.extendedUser, }) : super(key: key);
 
   @override
   _CardProfileScreenState createState() => _CardProfileScreenState();
 }
 
 class _CardProfileScreenState extends State<CardProfileScreen> {
-  UserService _userService;
-  bool _needsBootstrap;
+  
 
   @override
   void initState() {
-    _needsBootstrap = true;
     super.initState();
   }
 
@@ -49,9 +48,6 @@ class _CardProfileScreenState extends State<CardProfileScreen> {
     var themeValueParserService = openbookProvider.themeValueParserService;
     var toastService = openbookProvider.toastService;
 
-    if (_needsBootstrap) {
-      _userService = openbookProvider.userService;
-    }
 
 
     List<Container> _buildCategoriesList(CategoriesList _categories) {
@@ -85,154 +81,136 @@ class _CardProfileScreenState extends State<CardProfileScreen> {
       return _communitiesWidget;
     }
 
+    User _user = widget.extendedUser.extendedInformation;
+
     return Container(
       child: Center(
         child: ClipRRect(
           borderRadius: BorderRadius.circular(25),
           child: CupertinoPageScaffold(
             backgroundColor: Color.fromARGB(0, 0, 0, 0),
-            child: FutureBuilder<User>(
-              future: _userService.getUserWithUsername(widget.user.username),
-              builder: (context, AsyncSnapshot<User> snapshot){
-                switch (snapshot.connectionState) {
-                case ConnectionState.waiting: return LoadingContainer();
-                default:
-                  if (snapshot.hasError)
-                      {return Text('Error: ${snapshot.error}');}
-                  else{
-                    User _user = snapshot.data;
-                    return OBPrimaryColorContainer(
-                      child: Column(
-                        children:[
-                          Container(height: 80,child: OBProfileCover(_user)),
-                          Stack(
-                            clipBehavior: Clip.none,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(left: 18.0, right: 18),
-                                child: Column(
+            child: OBPrimaryColorContainer(
+              child: Column(
+                children:[
+                  Container(height: 80,child: OBProfileCover(_user)),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 18.0, right: 18),
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: (OBAvatar.AVATAR_SIZE_MEDIUM * 0.2),
+                              width: OBAvatar.AVATAR_SIZE_MEDIUM,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: 20,),
+                                _buildNameRow(user: _user, context: context, toastService: toastService),
+                                OBProfileUsername(_user),
+
+                                _user.profile.bio != null ? Container(
+                                  height: 65,
+                                  child: OBProfileBio(user: _user, size: OBTextSize.small,)
+                                ) : SizedBox(),
+
+                                _user.categories.categories.length >0 ?  Column(
                                   children: [
-                                    const SizedBox(
-                                      height: (OBAvatar.AVATAR_SIZE_MEDIUM * 0.2),
-                                      width: OBAvatar.AVATAR_SIZE_MEDIUM,
-                                    ),
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        const SizedBox(
-                                          height: 20,
+                                    const Divider(height: 20,),
+                                    Container(
+                                        height: 50,
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text('CATEGORIES', style: TextStyle(fontSize: 8),),
+                                            const SizedBox( height: 5),
+                                            Container(
+                                              height: 30,
+                                              child: ListView(
+                                                scrollDirection: Axis.horizontal,
+                                                children: _buildCategoriesList(_user.categories),
+                                              ),
+                                            ),
+                                          ],
                                         ),
-                                        _buildNameRow(user: _user, context: context, toastService: toastService),
-                                        OBProfileUsername(_user),
+                                      ),
+                                  ]) : SizedBox(),
 
-                                        _user.profile.bio != null ? Container(
-                                          height: 65,
-                                          child: OBProfileBio(_user)
-                                        ) : SizedBox(),
-
-                                        _user.categories.categories.length >0 ?  Column(
+                                _user.communitiesMemberships.communityMemberships.length > 0
+                                  ?  Column(
                                         children: [
-                                          const Divider(
-                                            height: 20,
-                                          ),
+                                          const Divider(height: 20,),
                                           Container(
-                                              height: 50,
-                                              child: Column(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                children: [
-                                                  Text('CATEGORIES', style: TextStyle(fontSize: 8),),
-                                                  const SizedBox( height: 5),
-                                                  Container(
-                                                    height: 30,
-                                                    child: ListView(
-                                                      scrollDirection: Axis.horizontal,
-                                                      children: _buildCategoriesList(_user.categories),
-                                                    ),
+                                            height: 50,
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text('GROUPS', style: TextStyle(fontSize: 8),),
+                                                const SizedBox( height: 5),
+                                                Container(
+                                                  height: 30,
+                                                  child: ListView(
+                                                    scrollDirection: Axis.horizontal,
+                                                    children: _buildCommunitiesList(_user.communitiesMemberships),
                                                   ),
-                                                ],
-                                              ),
+                                                ),
+                                              ],
                                             ),
+                                          ),
                                         ]) : SizedBox(),
 
-                                        _user.communitiesMemberships.communityMemberships.length > 0
-                                        ?  Column(
-                                        children: [
-                                          const Divider(
-                                            height: 20,
-                                          ),
-                                          Container(
-                                              height: 50,
-                                              child: Column(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                children: [
-                                                  Text('GROUPS', style: TextStyle(fontSize: 8),),
-                                                  const SizedBox( height: 5),
-                                                  Container(
-                                                    height: 30,
-                                                    child: ListView(
-                                                      scrollDirection: Axis.horizontal,
-                                                      children: _buildCommunitiesList(_user.communitiesMemberships),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                        ]) : SizedBox(),
+                                OBProfileDetails(_user),
+                                OBProfileCounts(_user),
 
-                                        OBProfileDetails(_user),
-                                        OBProfileCounts(_user),
+                              ]),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        child: StreamBuilder(
+                          stream: themeService.themeChange,
+                          initialData: themeService.getActiveTheme(),
+                          builder: (BuildContext context, AsyncSnapshot<OBTheme> snapshot) {
+                            var theme = snapshot.data;
 
-                                    ]),
-                                  ],
-                                ),
-                              ),
-                              Positioned(
-                                child: StreamBuilder(
-                                  stream: themeService.themeChange,
-                                  initialData: themeService.getActiveTheme(),
-                                  builder: (BuildContext context, AsyncSnapshot<OBTheme> snapshot) {
-                                    var theme = snapshot.data;
+                            return Container(
+                              height: 20,
+                              width: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                color: themeValueParserService.parseColor(theme.primaryColor),
+                                borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(50),
+                                topRight: Radius.circular(50))),
+                              );
+                            },
+                          ),
+                        top: -19,
+                      ),
+                      Positioned(
+                        top: -((OBAvatar.AVATAR_SIZE_MEDIUM/ 2)) - 10,
+                        left: 18,
+                        child: StreamBuilder(
+                          stream: _user.updateSubject,
+                          initialData: _user,
+                          builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+                            var user = snapshot.data;
 
-                                    return Container(
-                                      height: 20,
-                                      width: MediaQuery.of(context).size.width,
-                                      decoration: BoxDecoration(
-                                        color: themeValueParserService.parseColor(theme.primaryColor),
-                                        borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(50),
-                                        topRight: Radius.circular(50))),
-                                      );
-                                    },
-                                  ),
-                                top: -19,
-                              ),
-                              Positioned(
-                                top: -((OBAvatar.AVATAR_SIZE_MEDIUM/ 2)) - 10,
-                                left: 18,
-                                child: StreamBuilder(
-                                  stream: _user.updateSubject,
-                                  initialData: _user,
-                                  builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-                                    var user = snapshot.data;
-
-                                    return OBAvatar(
-                                      borderWidth: 3,
-                                      avatarUrl: user?.getProfileAvatar(),
-                                      size: OBAvatarSize.medium,
-                                      isZoomable: false,
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          )
-                        ]
-                      ),   
-                    );
-                  }
-                }
-              }    
-            ),
+                            return OBAvatar(
+                              borderWidth: 3,
+                              avatarUrl: user?.getProfileAvatar(),
+                              size: OBAvatarSize.medium,
+                              isZoomable: false,
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  )
+                ]
+              ),   
+            )
           ),
         ),
       ),
